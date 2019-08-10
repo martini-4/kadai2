3159a600c5a661341cf148a77635586c21443c2f
吉松拓巳 <matty@matty-MacBook-Pro.local>
2019-08-10 13:49:09 +0900

    start2

---
+++ vagrant/Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def index
+  
+  end
+
+  def new
+	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book =Book.new(book_params)
+  	book.save
+  	redirect_to '/books'
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+end
